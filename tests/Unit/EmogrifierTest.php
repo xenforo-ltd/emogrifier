6f8e5c4b4932a223cc8d620c7c5dd08ed93a41fa
Oliver Klee <github@oliverklee.de>
2018-01-12 15:35:31 +0100

    [BUGFIX] Automatically add missing DOCTYPE

--- tests/Unit/EmogrifierTest.php
+++ tests/Unit/EmogrifierTest.php
@@ -248,6 +248,18 @@ class EmogrifierTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @test
+     */
+    public function addMissingHtml5DocumentType()
+    {
+        $this->subject->setHtml('<html></html>');
+
+        $result = $this->subject->emogrifyBodyContent();
+
+        static::assertContains('<!DOCTYPE html>', $result);
+    }
+
+    /**
      * @return string[][]
      */
     public function documentTypeDataProvider()
